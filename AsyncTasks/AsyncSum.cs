19319dcc7d0ed4f795d365e263fbe70f3a3cccd1
Ivan Khromiak <ivan.khromiak@gmail.com>
2019-11-05 12:46:00 +0200

    Improvement of AsyncSum in AsyncTasks

--- AsyncTasks/AsyncSum.cs
+++ AsyncTasks/AsyncSum.cs
@@ -11,8 +11,9 @@ namespace AsyncTasks
     {
         public static int Sum(int[,] numbers)
         {
-            var parallelSums = new int[10];
+            var parallelSums = new int[11];
             int parts = numbers.GetLength(0) / 10;
+
             Parallel.For(0, 10, (counter) =>
             {
                 int sum = 0;
@@ -28,7 +29,23 @@ namespace AsyncTasks
                 parallelSums[counter] = sum;
             });
 
+            if (numbers.GetLength(0) % 10 == 0) return parallelSums.Sum();
+
+            var thread = new Thread(RemainsSum);
+            thread.Start();
+            thread.Join();
             return parallelSums.Sum();
+
+            void RemainsSum()
+            {
+                for (var i = parts * 10; i < numbers.GetLength(0); i++)
+                {
+                    for (var j = 0; j < numbers.GetLength(1); j++)
+                    {
+                        parallelSums[10] += numbers[i, j];
+                    }
+                }
+            }
         }
     }
 }
