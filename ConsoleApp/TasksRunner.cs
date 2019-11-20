0c7f00834c3d406a8b57d99b9de7c3031b315e16
Ivan Khromiak <ivan.khromiak@gmail.com>
2019-11-20 18:07:20 +0200

    Fix of TasksRunner

--- ConsoleApp/TasksRunner.cs
+++ ConsoleApp/TasksRunner.cs
@@ -1,32 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using InversionOfControl;
 using UserInterface;
+using NLog;
 
 namespace ConsoleApp
 {
     class TasksRunner
     {
-        private List<UserInterface.IRunnable> _runners = new List<UserInterface.IRunnable>();
+        private readonly List<UserInterface.IRunnable> _runners = new List<UserInterface.IRunnable>();
 
-        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly NLog.Logger _logger = LogManager.GetCurrentClassLogger();
 
         public TasksRunner()
         {
-            var container = new Container();
-            container.RegisterSingleton<IUserInterface, ConsoleUI>();
-
-            _runners.Add(new TasksStructs.Runner(container.GetInstance<ConsoleUI>()));
-            _runners.Add(new TasksEnums.Runner(container.GetInstance<ConsoleUI>()));
-            _runners.Add(new TasksExceptions.Runner(container.GetInstance<ConsoleUI>()));
-            _runners.Add(new IOTasks.RunnerIOTasks(container.GetInstance<ConsoleUI>(), _logger));
-            _runners.Add(new SerializationTasks.Runner(container.GetInstance<ConsoleUI>()));
-            _runners.Add(new ReflectionTasks.ReflectionTasksRunner(container.GetInstance<ConsoleUI>()));
+            _runners.Add(new TasksStructs.Runner(new ConsoleUI()));
+            _runners.Add(new TasksEnums.Runner(new ConsoleUI()));
+            _runners.Add(new TasksExceptions.Runner(new ConsoleUI()));
+            _runners.Add(new IOTasks.RunnerIOTasks(new ConsoleUI(), _logger));
+            _runners.Add(new SerializationTasks.Runner(new ConsoleUI()));
+            _runners.Add(new ReflectionTasks.ReflectionTasksRunner(new ConsoleUI()));
             _runners.Add(new Calculation.Runner());
             _runners.Add(new FileOperations.Runner(_logger));
             _runners.Add(new ExcelOperations.Runner(_logger));
-            _runners.Add(new AsyncTasks.Runner(container.GetInstance<ConsoleUI>()));
+            _runners.Add(new AsyncTasks.Runner(new ConsoleUI()));
         }
 
         public void Run(int choice)
